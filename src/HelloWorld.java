c12ace3fc005de9b1cf5b3d7c2ef2f845e660fc5
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 11:43:32 -0500

    3 of 14 completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,5 +12,28 @@ public class HelloWorld {
 //        to it, then print the variable out to the console.
         String myString = "Cookie";
         System.out.println(myString);
+
+//        3.Change your code to assign a character value to myString. What do you notice?
+//            myString = 'A'; You get an incompatible types
+
+//        4.Change your code to assign the value 3.14159 to myString. What happens?
+
+
+//        5.Declare an long variable named myNumber, but do not assign anything to it. Next
+//          try to print out myNumber to the console. What happens?
+
+
+//        6.Change your code to assign the value 3.14 to myNumber. What do you notice?
+
+
+//        7.Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
+
+//        8.Change your code to assign the value 123 to myNumber.
+
+
+//        9.hy does assigning the value 3.14 to a variable declared as a long not compile, but
+//          assigning an integer value does? Change your code to declare myNumber as a float. Assign
+//          the value 3.14 to it. What happens? What are two ways we could fix this?
     }
 }
