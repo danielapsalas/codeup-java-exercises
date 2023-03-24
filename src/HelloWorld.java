9cdc73f8910903e767d10f645c4bbc0ec566f211
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 11:54:39 -0500

    9 of 14 done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,18 +14,18 @@ public class HelloWorld {
         System.out.println(myString);
 
 //        3.Change your code to assign a character value to myString. What do you notice?
-//            myString = 'A'; You get an incompatible types
+//            myString = 'A'; //You get an incompatible types
 
 //        4.Change your code to assign the value 3.14159 to myString. What happens?
-//            myString = 3.14159; You get an incompatible types
+//            myString = 3.14159; //You get an incompatible types
 
 //        5.Declare an long variable named myNumber, but do not assign anything to it. Next
 //          try to print out myNumber to the console. What happens?
 //            long myNumber = ;
-//            System.out.println(myNumber); You get java: illegal start of expression
+//            System.out.println(myNumber); //You get java: illegal start of expression
 
 //        6.Change your code to assign the value 3.14 to myNumber. What do you notice?
-//            long myNumber = (long) 3.14; You have to put a cast
+//            long myNumber = (long) 3.14; //You have to put a cast
 
 //        7.Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
             float myNumber = 123L;
@@ -37,8 +37,9 @@ public class HelloWorld {
             myNumber  = 123;
             System.out.println(myNumber);
 
-//        9.hy does assigning the value 3.14 to a variable declared as a long not compile, but
+//        9.Why does assigning the value 3.14 to a variable declared as a long not compile, but
 //          assigning an integer value does? Change your code to declare myNumber as a float. Assign
 //          the value 3.14 to it. What happens? What are two ways we could fix this?
+//            myNumber = 3.14; //You have to put a cast when you use long
     }
 }
