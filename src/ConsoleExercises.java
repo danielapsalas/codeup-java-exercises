2cdeed5e66e5d49349b9987c8e73a75e89d98983
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 16:54:02 -0500

    2 of scanner class completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -16,12 +16,37 @@ public class ConsoleExercises {
         System.out.format("The value of pi is approximately %.2f", pi);
 
 //        Prompt a user to enter a integer and store that value in an int variable
-//        using the nextInt method.
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+////        using the nextInt method.
+        Scanner myNum = new Scanner(System.in);  // Create a Scanner object
         System.out.println("\nEnter a number: ");
 
-        int userNumber = myObj.nextInt();  // Read user input
+        int userNumber = myNum.nextInt();  // Read user input
         System.out.println("User number is: " + userNumber);  // Output user input
 
+
+
+//        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them
+//        back in the console, each on a newline.
+//
+//        What happens if you enter less than 3 words?
+//        What happens if you enter more than 3 words?
+
+        Scanner my1stWord = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\nEnter three words: ");
+
+        String oneWord = my1stWord.nextLine();  // Read user input
+        System.out.println("User 1st word: " + oneWord);  // Output user input
+
+        Scanner my2ndWord = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\nEnter three words: ");
+
+        String twoWord = my2ndWord.nextLine();  // Read user input
+        System.out.println("User 2nd word: " + twoWord);  // Output user input
+
+        Scanner my3rdWord = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\nEnter three words: ");
+
+        String thirdWord = my3rdWord.nextLine();  // Read user input
+        System.out.println("User 3rd word: " + thirdWord);  // Output user input
     }
 }
