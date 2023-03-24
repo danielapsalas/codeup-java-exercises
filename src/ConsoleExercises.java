59a9ebbf8bd12416646883c083db0382cd8c77e3
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 16:57:00 -0500

    3 of scanner class completed

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -15,8 +15,10 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.format("The value of pi is approximately %.2f", pi);
 
-//        Prompt a user to enter a integer and store that value in an int variable
-////        using the nextInt method.
+//        Explore the Scanner Class.
+
+//        1.Prompt a user to enter a integer and store that value in an int variable
+//        using the nextInt method.
         Scanner myNum = new Scanner(System.in);  // Create a Scanner object
         System.out.println("\nEnter a number: ");
 
@@ -25,28 +27,40 @@ public class ConsoleExercises {
 
 
 
-//        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them
+//        2.Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them
 //        back in the console, each on a newline.
 //
 //        What happens if you enter less than 3 words?
 //        What happens if you enter more than 3 words?
 
         Scanner my1stWord = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("\nEnter three words: ");
+        System.out.println("\nEnter first word: ");
 
         String oneWord = my1stWord.nextLine();  // Read user input
         System.out.println("User 1st word: " + oneWord);  // Output user input
 
         Scanner my2ndWord = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("\nEnter three words: ");
+        System.out.println("\nEnter second word: ");
 
         String twoWord = my2ndWord.nextLine();  // Read user input
         System.out.println("User 2nd word: " + twoWord);  // Output user input
 
         Scanner my3rdWord = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("\nEnter three words: ");
+        System.out.println("\nEnter third word: ");
 
         String thirdWord = my3rdWord.nextLine();  // Read user input
         System.out.println("User 3rd word: " + thirdWord);  // Output user input
+
+//        3.Prompt a user to enter a sentence, then store that sentence in a String variable
+//        using the next method. Then, display that sentence back to the user.
+//
+//        do you capture all the words?
+
+        Scanner userSentence = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("\nEnter a sentence: ");
+
+        String sentence = my1stWord.nextLine();  // Read user input
+        System.out.println("User sentence: " + sentence);  // Output user input
+
     }
 }
