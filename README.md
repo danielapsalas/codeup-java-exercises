af20a2fd8aaeacdfe616897e6501f28e53271cf6
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 11:35:44 -0500

    1 of 14 completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # codeup-java-exercises
-# codeup-java-exercises
+
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+    }
+}
