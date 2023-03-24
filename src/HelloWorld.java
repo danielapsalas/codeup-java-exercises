addbb92dc411b6034f30fbec82c1f31c76fb936e
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-24 11:39:20 -0500

    2 of 14 completed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,14 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
 
+//        1.Create an int variable named myFavoriteNumber and assign
+//        your favorite number to it, then print it out to the console.
         int myFavoriteNumber = 13;
         System.out.println(myFavoriteNumber);
+
+//        2.Create a String variable named myString and assign a string value
+//        to it, then print the variable out to the console.
+        String myString = "Cookie";
+        System.out.println(myString);
     }
 }
