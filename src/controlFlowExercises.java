19f51c981565ab662e190039f3f236caf0bff484
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-27 12:07:08 -0500

    Loop Basics - Do while loop: part 1 completed

--- src/controlFlowExercises.java
+++ src/controlFlowExercises.java
@@ -15,6 +15,24 @@ public class controlFlowExercises {
             System.out.print(" " + i);
             i++;
         }
+
+//        b.Do While
+//
+//        1.Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        2.Alter your loop to count backwards by 5's from 100 to -10.
+//        3.Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than
+//        1,000,000. Output should equal:
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
+        int x = 0;
+        do {
+            System.out.println(x);
+            x += 2;
+        } while (i <= 100);
     }
 }
 
