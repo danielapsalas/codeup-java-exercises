c034497e86a941a8ce12bcd2cc0a065e446ca20e
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-27 16:04:47 -0500

    first three strings added - String Basics

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,5 +1,32 @@
 public class StringExercise {
     public static void main(String[] args) {
+//        String Basics.
+//
+//        Create a class named StringExercise with a main method.
+//
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+//
+//
+//        We don't need no education
+//        We don't need no thought control
+//
+//        Check "this" out!, "s inside of "s!
+//
+//        In windows, the main drive is usually C:\
+//
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
 
+        String education = "We don't need no education";
+        String control = "We don't need no thought control";
+
+        String inside = "Check \"this\" out!, \"s inside of \"! ";
+
+        String windows = "In windows, the main drive is usually C:\\";
+
+        System.out.println(education);
+        System.out.println(control);
+        System.out.println(inside);
+        System.out.println(windows);
     }
 }
