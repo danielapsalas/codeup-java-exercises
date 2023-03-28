67ad55e81350fbeab3ec38427a42e6aeeceb3a5a
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-28 12:07:57 -0500

    dividing and modulus portion done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -11,10 +11,20 @@ public class MethodsExercises {
         return num1 * num2;
     }
 
+    public static double dividing(double num1, double num2){
+        return num1 / num2;
+    }
+
+    public static double modulus(double num1, double num2){
+        return num1 % num2;
+    }
+
     public static void main(String[] args){
         System.out.println("addition: " + adding(10, 1));
         System.out.println("subtraction: " + subtracting(10, 1));
         System.out.println("multiplying: " + multiplying(10, 1));
+        System.out.println("dividing: " + dividing(10, 1));
+        System.out.println("modulus: " + modulus(10, 1));
     }
 
 
