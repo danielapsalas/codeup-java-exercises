f1bfd9945b4bc7af472cf8e060184dfc796bbca6
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-28 12:04:46 -0500

    subtracting portion done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,8 +3,13 @@ public class MethodsExercises {
         return num1 + num2;
     }
 
+    public static double subtracting(double num1, double num2){
+        return num1 - num2;
+    }
+
     public static void main(String[] args){
-        System.out.println(adding(10, 1));
+        System.out.println("addition: " + adding(10, 1));
+        System.out.println("subtraction: " + subtracting(10, 1));
     }
 
 
