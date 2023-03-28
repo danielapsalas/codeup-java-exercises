688ce72d0eac52548722ced042a3fc5c4590b1d6
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-28 12:02:43 -0500

    adding portion done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,11 @@
 public class MethodsExercises {
+    public static double adding(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static void main(String[] args){
+        System.out.println(adding(10, 1));
+    }
+
 
 }
