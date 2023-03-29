13e65ee4bbe29f34c032d1c10501926959bd3cb1
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-29 15:29:04 -0500

    got errors for each block

--- src/Person.java
+++ src/Person.java
@@ -23,5 +23,30 @@ public class Person {
         System.out.println(user1.getName());
         System.out.println(user1.sayHello());
 
+        System.out.println("~~~~~~~~~~~");
+//
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//        //error: constructor Person in class Person cannot be applied
+//        // to given types;
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        //error: constructor Person in class Person cannot be applied
+//        // to given types;
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        //error: constructor Person in class Person cannot be applied
+//        // to given types;
+
     }
 }
