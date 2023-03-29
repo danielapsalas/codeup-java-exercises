8a289c4194900c4b6bebc3293c15a7d0f749e447
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-29 15:58:36 -0500

    Understanding references completed moving onto Create an input validation class

--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,37 @@
 public class Person {
+
+//    Object basics
+//
+//    Create Person class inside of src that has a private name field that is a string, and the following methods:
+//
+//
+//    public String getName(){
+//      //TODO: return the person's name
+//    }
+//
+//    public void setName(String name){
+//      //TODO: change the name field to the passed value
+//    }
+//    public void sayHello(){
+//      //TODO: print a message to the console using the person's name
+//    }
+//
+//    The class should have a constructor that accepts a `String` value and sets
+//    the person's name to the passed string.
+//
+//    Create a `main` method on the class that creates a new `Person` object and
+//    tests the above methods.
     private String name;
 
+    public Person (){}
+
+    public Person(String name){
+        this.name = name;
+    }
     public String getName() {
         //TODO: print a message to the console using the person's name
         return name;
     }
-
 
     public void setName(String name) {
     //TODO: change the name field to the passed value
@@ -24,29 +50,26 @@ public class Person {
         System.out.println(user1.sayHello());
 
         System.out.println("~~~~~~~~~~~");
-//
+
 //        Person person1 = new Person("John");
 //        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-//        //error: constructor Person in class Person cannot be applied
-//        // to given types;
-
+//        System.out.println(person1.getName().equals(person2.getName())); //true
+//        System.out.println(person1 == person2); //false
+//
+//        System.out.println("~~~~~~~~~~~");
+//        Person person3 = new Person("John");
+//        Person person4 = person1;
+//        System.out.println(person3 == person4);//false
+//
+//        System.out.println("~~~~~~~~~~~");
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
-//        System.out.println(person1 == person2);
-//        //error: constructor Person in class Person cannot be applied
-//        // to given types;
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
+//        System.out.println(person1.getName());//john
+//        System.out.println(person2.getName());//john
 //        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        //error: constructor Person in class Person cannot be applied
-//        // to given types;
+//        System.out.println(person1.getName());//jane
+//        System.out.println(person2.getName());//jane
+
 
     }
 }
