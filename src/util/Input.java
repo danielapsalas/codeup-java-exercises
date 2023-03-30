de975e23877c156199f51fa12224aea9f20a93c4
Daniela Salas <danielapiconsalas@gmail.com>
2023-03-30 09:56:56 -0500

    Create an input validation class done

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,55 @@
 package util;
 
+import com.sun.source.util.SourcePositions;
+
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner;
+
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+    public String getString(){
+        String stringInput = scanner.nextLine();
+        return stringInput;
+    }
+
+    public boolean yesNo(){
+        String booleanInput = scanner.nextLine();
+        if(booleanInput.toLowerCase().contains("yes")){
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+
+    int getInt(){
+        return this.scanner.nextInt();
+    }
+
+    int getInt(int min, int max){
+        int userNum;
+        do{
+            System.out.printf("Please enter a number between %d and %d", min, max);
+            userNum = this.scanner.nextInt();
+        }while(userNum > max || userNum < min);
+        return userNum;
+    }
+
+    double getDouble(){
+        return this.scanner.nextDouble();
+    }
+
+    double getDouble(double min, double max){
+        double userDouble;
+        do{
+            System.out.printf("Enter a decimal number from %f and %f", min, max);
+            userDouble = this.scanner.nextDouble();
+        }while(userDouble > max || userDouble < min);
+        return userDouble;
+    }
+
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,28 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input userStr = new Input();
+
+        System.out.println("enter a string: ");
+        System.out.println(userStr.getString());
+
+        System.out.println("Type in Yes or No:");
+        System.out.println(userStr.yesNo());
+
+        System.out.println("Enter an int:");
+        System.out.println(userStr.getInt());
+
+        System.out.println("Enter a number(int)");
+        System.out.println(userStr.getInt(1,10));
+
+        System.out.println("Enter a double:");
+        System.out.println(userStr.getDouble());
+
+        System.out.println("Enter a number(double)");
+        System.out.println(userStr.getDouble(1,10));
+
+
+
+    }
+}
