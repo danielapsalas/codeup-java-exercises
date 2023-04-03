81a49df413be56a85bc131441fd08e9997f40aa8
Daniela Salas <danielapiconsalas@gmail.com>
2023-04-03 16:35:56 -0500

    shapes exercise for Inheritance and polymorphism completed

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,7 +1,24 @@
 package shapes;
 
+import java.util.Arrays;
+
 public class ShapesTest {//Test your code by creating a new class, ShapesTest (also inside of shapes)
+
     public static void main(String[] args) {// with a main method.
-        Rectangle[]
+
+
+        //create a variable of the type Rectangle named box1 and assign it a new instance
+        // of the Rectangle class with a width of 4 and a length of 5
+        Rectangle box1 = new Rectangle(4, 5);
+        System.out.println("box1 rectangle area: " + box1.getArea());//verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+        System.out.println("box1 perimeter: " + box1.getPerimeter() + "\n");
+
+
+        //create a variable of the type Rectangle named box2 and assign it a new instance of
+        // the Square class that has a side value of 5.
+        Rectangle box2 = new Square(5);
+        System.out.println("box2 rectangle area: " + box2.getArea());//verify that the getPerimeter and getArea methods return 20 and 25, respectively.
+        System.out.println("box2 perimeter: " + box2.getPerimeter() + "\n");
+
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle{//Create a class named Square, also inside of shapes, that extends Rectangle
+    private double oneSide;
     public Square(double oneSide) {// Square should define a constructor that accepts one argument, side,
         super(oneSide, oneSide);//and calls the parent's constructor to set both the length and width to the value of side
+        this.oneSide = oneSide;
+    }
+
+    //In the Square class, override the getArea and getPerimeter methods with the following definitions for a square
+    @Override
+    public double getPerimeter(){
+        return ((oneSide * 4));
+    }
+
+    @Override
+    public double getArea(){
+        return (Math.pow(oneSide, 2));
     }
 }
 
