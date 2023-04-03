3e82de89d3f4c47520b6329c9cf200f107ff29ac
Daniela Salas <danielapiconsalas@gmail.com>
2023-04-03 10:06:13 -0500

    still working on array basics...

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,23 +1,31 @@
 import java.util.Arrays;
 public class ArraysExercises {
-    public static void main(String[] args) {
-        //        int[] numbers = {1, 2, 3, 4, 5};
-        //        System.out.println(numbers); //gives you the memory space
-        Person daniela = new Person("dani");
-        Person ayden = new Person("ayden");
-         Person fernando = new Person ("nando");
 
-        Person[] threePeople = {daniela, ayden, fernando};
-        for (Person onePerson: threePeople) {
-            System.out.println(onePerson.getName());
-        }
-
-
+    String personName;
+    static int personCount = 0;
+    public Person(String personName){
+        this.personName = personName; //Assign a unique String to this property
+        personCount++; //Count up this 'universal' / static property
     }
-//    public static Person[] addPerson(Person[] human, Person newHuman){
-//        human = Arrays.copyOf(human, human.length + 1);
-//        human[human.length - 1] = newHuman;
-//        return human;
-//    }
+    public static void main(String[] args) {
+        Person ayden = new Person("Ayden");
+        Person nohemi = new Person("Nohemi");
+        Person daniela = new Person("Daniela");
+        Person fernando = new Person("Fernando");
+        Person[] family = {ayden, nohemi, daniela, fernando}; //[0 = gene,1 = jenna, 2 = daniela,3 = gianna]
+        System.out.println(Arrays.toString(family));
+        Person[] newSquad = addPerson(family, new Person("Luis"));
+        //newSquad [0 = gene,1 = jenna, 2 = daniela,3 = gianna, 4= kenny] a length of FIVE with 0 - 4 as indices
+        for (Person hero : newSquad) {
+            System.out.printf("%s is my name %n", personName);
+        }
+    }
+
+
+    public static Person[] addPerson(Person[] incomingPersonArray, Person newPersonToAdd){
+        Person[] newPersonArray = Arrays.copyOf(incomingPersonArray, incomingPersonArray.length+1);
+        newPersonArray[incomingPersonArray.length] = newPersonToAdd;
+        return newPersonArray;
+    }
 
 }
