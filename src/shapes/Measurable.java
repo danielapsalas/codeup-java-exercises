fb143df41d89eccbd3bd4d97be8b4de7d598166d
Daniela Salas <danielapiconsalas@gmail.com>
2023-04-04 12:08:35 -0500

    1-3 completed and labeled

---
+++ src/shapes/Measurable.java
@@ -0,0 +1,8 @@
+package shapes;
+
+public interface Measurable {
+    //Inside of the shapes directory, create a Measurable interface with the following methods:
+    double getPerimeter();
+    double getArea();
+
+}
---
+++ src/shapes/Shape.java
@@ -0,0 +1,4 @@
+package shapes;
+
+public abstract class Shape { //Create an abstract Shape class inside of the shapes directory.
+}
